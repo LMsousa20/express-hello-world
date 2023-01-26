1aa027b3ed68e735342efc8141721ad18965ffab
lucas <lmsousa20@gmail.com>
2023-01-26 02:03:56 -0300

    cadastro-produto

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL
 })
 app.use(cors())
+app.use(express.json())
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => res.type('html').send(rows));
 
@@ -26,7 +30,7 @@ app.get('/users/:idclient', async (req, res) => {
   console.log(typeof client)
 
   try {
-      const retorno = await pool.query(`SELECT * FROM cliente WHERE cpf_cnpj='${client}'`)
+      const retorno = await pool.query(`SELECT * FROM clientes WHERE cpf='${client}'`)
       var reposta = res.status(200).send(retorno.rows)
 
       return reposta
@@ -38,14 +42,33 @@ app.get('/users/:idclient', async (req, res) => {
 
 app.get('/products', async (req, res) => {
   try {
-      const { rows } = await pool.query(`SELECT * FROM produtos`)
-      return res.status(200).send(rows)
+      const repProdutos = await pool.query(`SELECT * FROM cad_produtos`)
+      return res.status(200).send(repProdutos.rows)
   }
   catch (err) {
       return res.status(400).send(err)
   }
 })
 
+app.post('/cadprodutos', async (req, res)=>{
+const cad= {
+  nome: req.body.nome,
+  descricao: req.body.descricao,
+  valor: req.body.valor
+}
+console.log(cad)
+try {
+  await pool.query(`INSERT INTO cad_produtos (name, description, value_xpc)
+  VALUES('${cad.nome}', '${cad.descricao}', ${cad.valor});`)
+
+   return res.status(201).send(cad)
+}
+catch (err) {
+
+  return res.status(400).send(err)
+}
+
+})
 
 
 
